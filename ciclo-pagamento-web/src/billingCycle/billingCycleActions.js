f9efbe0c86ae0e3fcda3447b9f5077e0a0e4a67c
dsamuel32 <diego.samuel.alves@gmail.com>
2017-09-13 18:24:15 -0300

    Terminado formulario de incluir

--- ciclo-pagamento-web/src/billingCycle/billingCycleActions.js
+++ ciclo-pagamento-web/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,8 @@
 import axios from 'axios'
+import {toastr} from 'react-redux-toastr'
+import {reset as resetForm} from 'redux-form'
+
+import {showTabs, selectTab} from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:8080/api'
 
@@ -11,7 +15,25 @@ export function getList() {
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
+
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
+         .then(resp => {
+             toastr.success("Sucesso", "Operação Realizada com Sucesso")
+             dispatch([
+                 resetForm('billingCycleForm'),
+                 getList(),
+                 selectTab('tabList'),
+                 showTabs('tabList', 'tabCreate')
+             ])
+         })
+         .catch(e => {
+             const errors = e.response.data.errors
+             errors.forEach(error => toastr.error('Erro', error))
+         })
+    }
+
+    
     return {
         type: 'CREATE'
     }
