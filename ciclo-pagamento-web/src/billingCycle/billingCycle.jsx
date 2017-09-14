5ba513d99c5548d880e8d14a61f28ef85f3a1cf2
dsamuel32 <diego.samuel.alves@gmail.com>
2017-09-14 18:18:28 -0300

    Criado função add creditos

--- ciclo-pagamento-web/src/billingCycle/billingCycle.jsx
+++ ciclo-pagamento-web/src/billingCycle/billingCycle.jsx
@@ -9,16 +9,14 @@ import TabsContent from    '../common/tab/tabsContent'
 import TabContent from    '../common/tab/tabContent'
 import TabsHeader from    '../common/tab/tabsHeader'
 import TabHeader from    '../common/tab/tabHeader'
-import {selectTab, showTabs} from '../common/tab/tabActions'
 import BillingCycleList from './billingCycleList'
 import BillingCycleForm from  './billingCycleForm'
-import {create, update, remove} from './billingCycleActions'
+import {init, create, update, remove} from './billingCycleActions'
 
 class BillingCycle extends Component {
 
     componentDidMount() {
-        this.props.selectTab('tabList')
-        this.props.showTabs('tabList', 'tabCreate')
+        this.props.init()
     }
 
     render() {
@@ -60,7 +58,7 @@ class BillingCycle extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({selectTab, showTabs, create, update, remove}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({init, create, update, remove}, dispatch)
 
 export default connect(null, mapDispatchToProps) (BillingCycle)
 
