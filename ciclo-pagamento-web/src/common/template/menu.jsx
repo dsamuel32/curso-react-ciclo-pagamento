ff4163ea7292e0f0b00fbe061e47e264e43b29cf
dsamuel32 <diego.samuel.alves@gmail.com>
2017-09-15 17:07:26 -0300

    Alterando menu e links de rotas.

--- ciclo-pagamento-web/src/common/template/menu.jsx
+++ ciclo-pagamento-web/src/common/template/menu.jsx
@@ -5,9 +5,9 @@ import MenuTree from './menuTree'
 
 export default propos => (
     <ul className="sidebar-menu">
-        <MenuItem path="#" icon="dashboard" label="DashBoard"/>
+        <MenuItem path="/" icon="dashboard" label="DashBoard"/>
         <MenuTree label="Cadastro" icon="edit">
-            <MenuItem path="#billingCycles" label="Ciclos de Pagamento" icon="usd"/>
+            <MenuItem path="billingCycles" label="Ciclos de Pagamento" icon="usd"/>
         </MenuTree>
     </ul>
 )
